097c0d11ddf258a424acfc47a58d7dd28d63d29e
Ravi Kumar CSE (202115) <90110167+kr99ravi@users.noreply.github.com>
2022-04-13 13:11:02 +0530

    Create function power.cpp

---
+++ function power.cpp
@@ -0,0 +1,40 @@
+//Use of function power
+//Ravi Kumar , Roll no. - 202115
+#include<iostream>
+using namespace std;
+class power { 
+     private: 
+     double m,result = 1; 
+     int n; 
+     
+     public : void takefunction() 
+     {
+         cout<<"enter the integer"<<endl;
+         cin>>m;
+         cout<<"enter the power"<<endl;
+         cin>>n; 
+         } 
+         
+         void displaydata() 
+         {
+              cout<<"your result is "<<result<<endl;
+               } 
+               void powerfun()
+               { 
+                   for(int i=1;i<=n;i++)
+                   {
+                        result=result*m; 
+                       
+                   }
+                } 
+         }; 
+         
+         int main() { 
+            power s1; 
+            s1.takefunction(); 
+            s1.powerfun(); 
+            s1.displaydata();
+                              
+        return 0; 
+                             
+         }
